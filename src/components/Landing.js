9d57513a23d78da197a25702c7b9ad177d7f37bb
Natalia <nataliostapiuk@gmail.com>
2020-12-28 01:03:11 +0200

    button added

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,14 +6,14 @@ class Landing extends Component {
         super()
         this.state = {
             users: [{ id: 1, name: 'user1', budget: 10, rentedMovies: [] }, { id: 2, name: 'user2', budget: 15, rentedMovies: [] },
-            { id: 3, name: 'user3', budget: 3, rentedMovies: [] },]
+            { id: 3, name: 'user3', budget: 3, rentedMovies: [] },  { id: 3, name: 'user4', budget: 3, rentedMovies: [] }]
         }
     }
 
     render() {
         return (
             <div class='users'>{this.state.users.map(u => {
-                return <Link to="/catalog"><div className='user' key={Math.random()}> {u.name}</div></Link>
+                return <Link className='user' to="/catalog"><div  key={Math.random()}> {u.name}</div></Link>
             })}</div>
         )
     }
